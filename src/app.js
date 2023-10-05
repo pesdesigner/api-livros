20912dedf122cbd3f1b872707bb146cd87f8bc95
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2023-10-04 23:34:34 -0300

    adicionando métodos atualizar e deletar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import conectaNaDataBase from "./config/dbConnect.js";
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão realizada")
+})
 
 const app = express();
 app.use(express.json());
@@ -14,6 +25,12 @@ const livros = [
     },
 ]
 
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
@@ -22,9 +39,26 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 })
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso!");
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro excluído com sucesso!");;
 })
 
 export default app;
