3d8640c81f934ea7318a358749e4b9c57ca28784
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2023-10-05 23:11:27 -0300

    Gerando CRUD com banco de dados

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req,res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Falha ao listar livros`})
+        }        
+    }
+
+    static async listarLivroPorId (req,res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Falha ao listar livro`})
+        }        
     }
 
     static async cadastrarLivro (req, res) {
@@ -14,6 +28,26 @@ class LivroController {
         } catch (error) {
             res.status(500).json({ message: `${error.message} - Falha ao cadastrar livro`})
         }
+    }
+
+    static async atualizarLivro (req,res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado"});
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Falha ao atualizar livro`})
+        }        
+    }
+
+    static async excluirLivro (req,res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: "Livro excluído"});
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - Falha ao excluir livro`})
+        }        
     }
 };
 
