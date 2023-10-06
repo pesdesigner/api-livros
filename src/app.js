1ea5de3c8684bbde192f1e8f6db56909f8220998
Paulo Eduardo <pauloeduardo.dg@outlook.com>
2023-10-05 22:49:08 -0300

    Criando controles e rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conectaNaDataBase from "./config/dbConnect.js";
-import livro from "./models/Livro.js";
+import routes from "./routes/index.js"
 
 const conexao = await conectaNaDataBase();
 
@@ -13,17 +13,7 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js");
-});
-
-app.get("/livros", async (req, res) => {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
-})
+routes(app);
 
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
